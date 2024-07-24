efa3a2d498e0be3f32b059d47e7776def0935479
FauzanHacker97 <135342426+FauzanHacker97@users.noreply.github.com>
2024-07-24 15:35:53 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+document.querySelector('.heart').addEventListener('click', function() {
+    document.querySelector('button').style.display = 'block';
+    document.querySelector('h1').innerText = 'Now click the button';
+});
+
 function showConfession() {
     document.getElementById('confession').style.display = 'block';
+    document.querySelector('button').style.display = 'none';
 }
