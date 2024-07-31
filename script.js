0174a2534059f8d2b47f4eb20a8aa4545b54fed0
FauzanHacker97 <135342426+FauzanHacker97@users.noreply.github.com>
2024-07-31 20:31:24 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
-document.querySelector('.heart').addEventListener('click', function() {
-    document.querySelector('button').style.display = 'block';
-    document.querySelector('h1').innerText = 'Now click the button';
+// press the button to take a photo.
+
+let light = document.getElementById('circle');
+
+document.getElementById('button').addEventListener('click', () => {
+  light.classList.remove('flash-animation');
+  setTimeout(function(){
+  light.classList.add('flash-animation');
+  },500);
 });
 
-function showConfession() {
-    document.getElementById('confession').style.display = 'block';
-    document.querySelector('button').style.display = 'none';
-}
+let eject = document.getElementById('photo');
+
+document.getElementById('button').addEventListener('click', () => {
+  eject.classList.remove('eject-photo');
+  setTimeout(function(){
+  eject.classList.add('eject-photo');
+  },500);
+});
+
+$(document).ready(function () {
+    $('.container').mouseenter(function () {
+        $('.card').stop().animate({
+            top: '-90px'
+        }, 'slow');
+    }).mouseleave(function () {
+        $('.card').stop().animate({
+            top: 0
+        }, 'slow');
+    });
+});
+s
